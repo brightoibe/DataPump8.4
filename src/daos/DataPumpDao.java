8a0ddcf66b872f69fbbaa990c3a1c33fb6de2cd7
The Bright <The Bright@BRIGHTOIBE>
2019-11-11 23:15:07 +0100

    Remove dormant files

--- src/daos/DataPumpDao.java
+++ src/daos/DataPumpDao.java
@@ -2037,13 +2037,13 @@ public class DataPumpDao implements model.datapump.DataAccess {
         return obs;
     }
 
-    public int getObsIDOfConceptInList(int conceptID, int formID, List<model.datapump.Obs> obsList, Date visitDate) {
+    public int getObsGroupIDOfConceptInList(int conceptID, int formID, List<model.datapump.Obs> obsList, Date visitDate) {
         Obs obs = getConceptForForm(conceptID, formID, obsList);
-        int obsID = 0;
+        int obsGroupID = 0;
         if (obs != null) {
-            obsID = obs.getObsID();
+            obsGroupID = obs.getObsGroupID();
         }
-        return obsID;
+        return obsGroupID;
 
     }
 
@@ -2558,7 +2558,7 @@ public class DataPumpDao implements model.datapump.DataAccess {
                      This block of code extracts duration; durationUnit;
                      from the Obs List.
          */
-        int obsGroupID = getObsIDOfConceptInList(165724, 27, obsList, visitDate);
+        int obsGroupID = getObsGroupIDOfConceptInList(165724, 27, obsList, visitDate);
         System.out.println("Obs ID of ARV Medication: "+obsGroupID);
         obsPin = getConceptForFormInGroup(159368, 27, obsList, visitDate, obsGroupID);
         System.out.println("Prescribed regimen duration: "+obsPin.getValueNumeric());
