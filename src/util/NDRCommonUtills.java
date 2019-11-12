e5035ce0e8fe300189e75009189ba80caca8adc2
The Bright <The Bright@BRIGHTOIBE>
2019-11-12 02:20:14 +0100

    Remove dormant files

--- src/util/NDRCommonUtills.java
+++ src/util/NDRCommonUtills.java
@@ -97,9 +97,9 @@ public class NDRCommonUtills {
                 dayVal = duration * 7;
             }
             
-            if (dayVal > 120) {
-                dayVal = 30;
-            }
+            //if (dayVal > 120) {
+                //dayVal = 30;
+            //}
         }
         DateTime startDateTime = new DateTime(startDate);
         DateTime stopDateTime = startDateTime.plusDays(dayVal);
